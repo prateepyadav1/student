6ba62142ff2e94530d320936321a591acd42e76e
Manoj <amanoj9535@gmail.com>
2023-09-07 21:14:44 +0530

    adding diff.java

---
+++ ManojKumar/diff.java
@@ -0,0 +1,14 @@
+// Importing required classes
+import java.io.*;
+
+// Main class
+class GFG {
+
+	// Main driver method
+	public static void main(String[] args)
+	{
+		// Print statement
+		System.out.println(
+			"Welcome to GeeksforGeeks Learning");
+	}
+}
